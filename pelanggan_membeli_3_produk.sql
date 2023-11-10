6f3f238397ce2ee63f78c45222ba913e425090cd
farid <valkanid12@gmail.com>
2023-11-10 22:16:33 +0700

    pelanggan_membeli_3_produk

---
+++ pelanggan_membeli_3_produk.sql
@@ -0,0 +1,3 @@
+INSERT INTO Orders (Users_Id, Products_Id, Jumlah, Tanggal) VALUES (1, 1, 3, current_date());
+INSERT INTO Orders (Users_Id, Products_Id, Jumlah, Tanggal) VALUES (1, 2, 3, current_date());
+INSERT INTO Orders (Users_Id, Products_Id, Jumlah, Tanggal) VALUES (1, 3, 3, current_date());
